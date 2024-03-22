fbe5deb663f998c1338f77ac5d1f62a603d9f80c
Klaus Zimmermann <klaus.zimmermann@quansight.com>
2024-03-22 10:04:51 +0100

    cmake debugging

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+set -xeuo pipefail
+
 mkdir build
 cd build
 
-cmake ${CMAKE_ARGS} ..
+cmake -LAH --debug-find --log-level=DEBUG ${CMAKE_ARGS} ..
 
 make VERBOSE=1 -j${CPU_COUNT}
 make install
